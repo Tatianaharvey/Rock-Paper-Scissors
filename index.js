06c9231d26cd7aac851ad161f6cff4340a36c387
Tatiana <110640811+Tatianaharvey@users.noreply.github.com>
2022-08-31 14:36:14 -0700

    add functions

--- index.js
+++ index.js
@@ -14,28 +14,43 @@ function getComputerChoice() {
 }    
 
 
-function playGame(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection) {
         return "It's a tie!";
-    } else if (playerSelection === 'rock') {
+    } else if (playerSelection.toLowerCase() === 'rock') {
         if (computerSelection === 'paper') {
             return "You lose! Paper beats rock!"
         } else {
             return "You won! Rock beats scissors."
         }
-    } else if (playerSelection === 'paper') {
+    } else if (playerSelection.toLowerCase() === 'paper') {
         if (computerSelection === 'scissors') {
             return "Sorry, you lose. Scissors beats paper."
         } else {
             return "You won! Paper beats rock!"
         }
-    } else if (playerSelection === 'scissors') {
+    } else if (playerSelection.toLowerCase() === 'scissors') {
         if (computerSelection === 'rock') {
             return "Oops, you lose! Rock beats scissors."
         } else {
             return "Yahoo, you win! Scissors beats paper."
         }
     }
-
 } 
-console.log(playGame());
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound(playerSelection, computerSelection);
+        if (playerSelection > computerSelection) {
+            console.log("You win!");
+        } else {
+            console.log("Sorry, you lost to the computer.")
+        }   
+}
+}
+
+
+
+const playerSelection = prompt("Rock, Paper, or Scissors?")
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection))
