7fe7a9d1260416db220cac1b2d1db2fca7b3acc7
Tatiana <110640811+Tatianaharvey@users.noreply.github.com>
2022-08-31 20:39:00 -0700

    Fix bugs

--- index.js
+++ index.js
@@ -14,43 +14,55 @@ function getComputerChoice() {
 }    
 
 
-function playRound(playerSelection, computerSelection) {
+function playRound() {
+    const computerSelection = getComputerChoice();
+    const playerSelection =  prompt("Rock, Paper, or Scissors?");
     if (playerSelection.toLowerCase() === computerSelection) {
         return "It's a tie!";
     } else if (playerSelection.toLowerCase() === 'rock') {
         if (computerSelection === 'paper') {
+            playerScore += 1;
             return "You lose! Paper beats rock!"
         } else {
+            computerScore += 1;
             return "You won! Rock beats scissors."
         }
     } else if (playerSelection.toLowerCase() === 'paper') {
         if (computerSelection === 'scissors') {
+            playerScore += 1;
             return "Sorry, you lose. Scissors beats paper."
         } else {
+            computerScore += 1;
             return "You won! Paper beats rock!"
         }
     } else if (playerSelection.toLowerCase() === 'scissors') {
         if (computerSelection === 'rock') {
+            playerScore += 1;
             return "Oops, you lose! Rock beats scissors."
         } else {
+            computerScore += 1;
             return "Yahoo, you win! Scissors beats paper."
         }
     }
 } 
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playRound(playerSelection, computerSelection);
-        if (playerSelection > computerSelection) {
-            console.log("You win!");
-        } else {
-            console.log("Sorry, you lost to the computer.")
-        }   
-}
-}
+function game(maxScore) {
+    playerScore = 0;
+    computerScore = 0;
+    for (let i = 0; i <= maxScore; i++) {
+    console.log(playRound());
+      if (i === maxScore) {
+        if (playerScore > computerScore) {
+        console.log("You won the game!");
+      } else if (playerScore < computerScore) {
+        console.log("You lost the game.");
+      } else {
+      }
+        }
+     }
+    }
 
-
-
-const playerSelection = prompt("Rock, Paper, or Scissors?")
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection))
+console.log(game(4));
+// const computerSelection = getComputerChoice();
+// const playerSelection = prompt("Rock, Paper, or Scissors?");
+// console.log(playRound(playerSelection, computerSelection));
